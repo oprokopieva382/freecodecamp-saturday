5ee3e14d545b5a0cde7f909fdeb4cc409f35aace
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 14:03:53 -0600

    Arguments Passed in the Wrong Order

--- script.js
+++ script.js
@@ -128,3 +128,11 @@ function getNine() {
 
 let results = getNine();
 console.log(results);
+//Catch Arguments Passed in the Wrong Order When Calling a Function
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
