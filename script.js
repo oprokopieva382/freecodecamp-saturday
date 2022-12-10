1d645441be4142b40bc9fde5ada3af7e7a91ffc6
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 13:50:36 -0600

    Differences between the fCC and Browser Console

--- script.js
+++ script.js
@@ -84,3 +84,8 @@ a++;
 console.log(a);
 let sumAB = a + b;
 console.log(sumAB);
+//understanding the Differences between the freeCodeCamp and Browser Console
+let output =
+  "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
