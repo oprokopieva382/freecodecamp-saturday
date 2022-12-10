d3b4b63e68985312eecd8fc3c97ba17c089d8bca
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 13:18:58 -0600

    Match Ending String Patterns

--- script.js
+++ script.js
@@ -5,6 +5,10 @@
 //Find One or More Criminals in a Hunt
 let reCriminals = /C+/;
 //Match Beginning String Patterns
-let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /^Cal/;
-let result = calRegex.test(rickyAndCal);
+// let rickyAndCal = "Cal and Ricky both like racing.";
+// let calRegex = /^Cal/;
+// let result = calRegex.test(rickyAndCal);
+//Match Ending String Patterns
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/;
+let result = lastRegex.test(caboose);
