3eda96edbe2aebc45980f9bd1f7230bf5da698ad
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 13:16:18 -0600

    Match Beginning String Patterns

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let text = "<h1>Winter is coming</h1>";
-let myRegex = /<.*?>/;
-let result = text.match(myRegex);
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<.*?>/;
+// let result = text.match(myRegex);
 //
 //Find One or More Criminals in a Hunt
 let reCriminals = /C+/;
+//Match Beginning String Patterns
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/;
+let result = calRegex.test(rickyAndCal);
