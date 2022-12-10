03af9a44afa5e33391e94a29d4043be201ece6fa
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 13:23:17 -0600

    Match All Letters and Numbers

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ let reCriminals = /C+/;
 // let calRegex = /^Cal/;
 // let result = calRegex.test(rickyAndCal);
 //Match Ending String Patterns
-let caboose = "The last car on a train is the caboose";
-let lastRegex = /caboose$/;
-let result = lastRegex.test(caboose);
+// let caboose = "The last car on a train is the caboose";
+// let lastRegex = /caboose$/;
+// let result = lastRegex.test(caboose);
+//Match All Letters and Numbers
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g;
+let result = quoteSample.match(alphabetRegexV2).length;
