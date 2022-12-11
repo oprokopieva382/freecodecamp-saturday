3aa71b2742427fe872d626137c63a62a2c7f12fd
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 23:16:38 -0600

    spreadOut

--- script2.js
+++ script2.js
@@ -53,3 +53,11 @@ function copyMachine(arr, num) {
   return newArr;
 }
 console.log(copyMachine([true, false, true], 2));
+//
+function spreadOut() {
+  let fragment = ["to", "code"];
+  let sentence = ["learning", ...fragment, "is", "fun"];
+  return sentence;
+}
+
+console.log(spreadOut());
