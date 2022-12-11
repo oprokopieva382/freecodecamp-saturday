23ee73c6af8321fa87a210a45bdc90143b25396f
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 23:25:25 -0600

    filteredArray

--- script2.js
+++ script2.js
@@ -65,3 +65,24 @@ function quickCheck(arr, elem) {
   return arr.indexOf(elem) >= 0 ? true : false;
 }
 console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+//
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    3
+  )
+);
