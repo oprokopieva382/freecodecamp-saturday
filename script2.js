14f2fe9a930c9f0130105ce639830291878077c4
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 23:01:51 -0600

    splice

--- script2.js
+++ script2.js
@@ -17,3 +17,7 @@ function popShift(arr) {
   return [shifted, popped];
 }
 console.log(popShift(["challenge", "is", "not", "complete"]));
+//
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(1, 4);
+console.log(arr);
