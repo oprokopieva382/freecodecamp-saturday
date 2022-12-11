a036072095fd1e77043cccbeca020fafa51e4cf7
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 23:14:09 -0600

    copyMachine

--- script2.js
+++ script2.js
@@ -43,3 +43,13 @@ function forecast(arr) {
 console.log(
   forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
 );
+//
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+}
+console.log(copyMachine([true, false, true], 2));
