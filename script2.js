fc21b3fc8d360c9d67af064453cd852a330efd56
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 23:10:55 -0600

    forecast

--- script2.js
+++ script2.js
@@ -35,3 +35,11 @@ console.log(
     "FireBrick",
   ])
 );
+//
+function forecast(arr) {
+  let newArray = arr.slice(2, 4);
+  return newArray;
+}
+console.log(
+  forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
+);
