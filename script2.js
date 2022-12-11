db1dcc412695aaf7b3c69e8eb2554694bb6eea21
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 22:56:09 -0600

    mixedNumbers

--- script2.js
+++ script2.js
@@ -3,3 +3,10 @@ let yourArray = ["one", 2, "three", true, false, undefined];
 let myArray = ["a", "b", "c", "d"];
 myArray[1] = "bob";
 console.log(myArray);
+//
+function mixedNumbers(arr) {
+  arr.push(7, "VIII", 9);
+  arr.unshift("I", 2, "three");
+  return arr;
+}
+console.log(mixedNumbers(["IV", 5, "six"]));
